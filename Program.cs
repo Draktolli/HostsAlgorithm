6cc06d2b6af4682fc8aa2db5ee8cf436fc2b1bcc
Draktolli <t.koverin@gmail.com>
2023-04-06 20:03:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HostsAlgorithm;
+
+string[] A = new[] { "unlock.microvirus.md", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us" };
+string[] B = new[] { "microvirus.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us" };
+int[] result = Solution.solution(A, B);
+foreach(int i in result)
+{
+	Console.WriteLine(i);
+}
---
+++ Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostsAlgorithm
+{
+	public static class Solution
+	{
+		public static int[] solution(string[] A, string[] B)
+		{
+			List<string> forbidenHosts = new(B);
+			List<int> result = new();
+
+			for (int i = 0; i < A.Length; i++)
+			{
+				bool passed = true;
+				string[] subDomains = A[i].Split('.');
+				string currentHost = "";
+				for (int j = subDomains.Length - 1; j > -1; j--)
+				{
+					currentHost = subDomains[j] + "." + currentHost;
+					if (forbidenHosts.Contains(currentHost.TrimEnd('.')))
+					{
+						passed = false;
+						break;
+					}
+				}
+				if (passed)
+				{
+					result.Add(i);
+				}
+			}
+
+			return result.ToArray();
+		}
+	}
+}
